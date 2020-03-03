61be81b2b277fc083224cc79a430ed21ee4901d8
BaLLoNTV <50710411+BaLLoNTV@users.noreply.github.com>
2020-03-03 20:14:41 +0300

    Add files via upload

--- project/javascript/mainmenu.js
+++ project/javascript/mainmenu.js
@@ -26,3 +26,32 @@ function GetSingUpMenu() {
   document.getElementById("singup").style.display = "block";
   document.getElementById("switcher2").style.display = "none";
 }
+
+function SingUp() {
+	var Login = document.getElementById("unames").value;
+	var Password1 = document.getElementById ("psws").value;
+	var Password2 = document.getElementById ("psw-repeats").value;
+    fetch('/auth/signup', {
+        method: "POST",
+        body: JSON.stringify({login: Login, pwd1: Password1, pwd2: Password2})
+    })
+        .then(resp => {
+            console.log('resp:', resp);
+        }).catch(err => {
+            console.log('err:', err)
+        });
+}
+
+function LogIn() {
+	var Login = document.getElementById("uname").value;
+	var Password = document.getElementById ("psw").value;
+    fetch('/auth/login', {
+        method: "POST",
+        body: JSON.stringify({login: Login, pwd: Password})
+    })
+        .then(resp => {
+            console.log('resp:', resp);
+        }).catch(err => {
+            console.log('err:', err)
+        });
+}
